13500ccdd87452be1e4ea084e795fb1c0bf371c0
Dan Pickett <dpickett@enlightsolutions.com>
2010-03-23 16:03:11 -0400

    change rake tasks to configurable declaration
    can take a configuration yml file as a param now

--- lib/pyramid_scheme/tasks.rb
+++ lib/pyramid_scheme/tasks.rb
@@ -1,1 +1,34 @@
-load(File.join(File.dirname(__FILE__), 'tasks/rake_tasks.rake'))
+module PyramidScheme
+  # PyramidScheme::Tasks.new('path/to/configuration/yml')
+  class Tasks < ::Rake::TaskLib
+    attr_reader :yml_path
+
+    def initialize(yml_path = nil)
+      unless yml_path.nil?
+        @yml_path = yml_path 
+        configure_with_yml
+      end
+      define
+    end
+
+    protected
+    def configure_with_yml
+      PyramidScheme.configure_with_yml(@yml_path)
+    end
+
+    def define
+      namespace :pyramid_scheme do
+        desc "retrieve new sphinx indexes as the client"
+        task :retrieve do
+          PyramidScheme::IndexClient.new.retrieve_index
+        end
+
+        desc "create new sphinx indexes as the server"
+        task :index do
+          PyramidScheme::IndexServer.new.index
+        end
+      end
+    end
+    
+  end
+end
